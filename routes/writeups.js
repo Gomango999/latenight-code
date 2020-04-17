4c860f28a540413ea9c4153096a419f9a27ab092
Gomango999 <kv.zhu999@gmail.com>
2020-04-18 00:13:57 +1000

    Added a Writeup List Page

--- routes/writeups.js
+++ routes/writeups.js
@@ -3,5 +3,8 @@ var router = express.Router();
 var writeupsController = require('../controllers/writeupsController');
 
 router.get('/', writeupsController.index);
+writeupsController.writeups.forEach(writeup => {
+    router.get(writeup.url, writeupsController.renderer[writeup.url]);
+});
 
 module.exports = router;

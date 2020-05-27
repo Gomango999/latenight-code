6caf37ce4bc5ad0cf77e696d07d05088f78d48bc
Gomango999 <kv.zhu999@gmail.com>
2020-05-28 00:14:40 +1000

    Styled Blog List Page

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,5 +1,10 @@
+blogList = [
+  "First Blog",
+  "second Blog",
+  "yes",
+]
 exports.index = function (req, res) {
     res.render('blog_list.pug', {
-        writeups: exports.writeups,
+        blogList: blogList,
     });
 }

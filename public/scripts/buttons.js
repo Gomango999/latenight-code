becffa99395c8ea3c7dc78f92965bab3efd7555d
Gomango999 <kv.zhu999@gmail.com>
2020-06-06 18:58:19 +1000

    Added Relative File Paths

--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -1,11 +1,19 @@
 
 $(document).ready(function() {
-	const projectLink = 'http://localhost:8080/projects/'
-	const writeupLink = 'http://localhost:8080/writeups/'
-	const artLink = 'http://localhost:8080/art/'
-	const blogLink = 'http://localhost:8080/blog/'
+	const projectLink = './projects/'
+	const writeupLink = './writeups/'
+	const artLink = './art/'
+	const blogLink = './blog/'
 
 	// Attach listeners to each button
+	$('#button-blog').on('click', function(e) {
+		const url = blogLink;
+	  	if (e.ctrlKey){
+			window.open(url,'_blank')
+		} else {
+			document.location.href = url;
+		}
+	})
 	$('#button-project').on('click', function(e) {
 		const url = projectLink;
 	  	if (e.ctrlKey){
@@ -24,14 +32,6 @@ $(document).ready(function() {
 	})
 	$('#button-art').on('click', function(e) {
 		const url = artLink;
-	  	if (e.ctrlKey){
-			window.open(url,'_blank')
-		} else {
-			document.location.href = url;
-		}
-	})
-	$('#button-blog').on('click', function(e) {
-		const url = blogLink;
 	  	if (e.ctrlKey){
 			window.open(url,'_blank')
 		} else {

1085cab485abcb225e9cbe2bb0637f8099b9b892
Gomango999 <kv.zhu999@gmail.com>
2021-03-10 01:15:57 +1100

    Added menu and made blog responsive

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -18,29 +18,37 @@ files = [
   '08_triangle.json',
 ]
 
-// Generate blogs list, and populate it with information
+// generate blogs list, and populate it with information
 let blogs = []
 files.forEach(filename => {
+  // get json data
   rawText = fs.readFileSync(path.join('./public/files/blog_posts/', filename), 'utf-8');
   blog = JSON.parse(rawText);
+
+  // get blog content and convert from markdown
+  let rawMarkdown = fs.readFileSync(path.join('./public/files/blog_posts/', blog.name + '.md'), 'utf-8');
+
   const converter = new showdown.Converter({
     extensions: [showdownKatex({
       delimiters: [{ left: '$', right: '$', asciimath: false }],
     })]
   });
-  let rawMarkdown = fs.readFileSync(path.join('./public/files/blog_posts/', blog.name + '.md'), 'utf-8');
+
   blog.content = converter.makeHtml(rawMarkdown);
+
+  // add blog metadata
   blog.url = '/'+blog.name;
   blog.timeFromUpload = moment(blog.uploadDate).fromNow();
+
   blogs.push(blog);
 });
 
-// Sort blogs in reverse chronological order
+// sort blogs in reverse chronological order
 blogs.sort((a, b) => {
   return moment(a.uploadDate).isBefore(moment(b.uploadDate)) ? 1 : -1;
 });
 
-// Add renderers to each blog
+// add renderers to each blog
 blogs.forEach(blog => {
   blog.renderer = function (req, res) {
     res.render('blog.pug', { blog: blog });

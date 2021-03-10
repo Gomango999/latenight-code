9d26c5de99c0e32ced1b2525fa30841c9389ba3a
Gomango999 <kv.zhu999@gmail.com>
2021-03-10 18:02:14 +1100

    Fixed homepage and blog list
    Added responsiveness to both as well

--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -1,37 +1,11 @@
 
 $(document).ready(function() {
-	const projectLink = './projects/'
-	const writeupLink = './writeups/'
-	const artLink = './art/'
 	const blogLink = './blog/'
 
 	// Attach listeners to each button
 	$('#button-blog').on('click', function(e) {
+    console.log("HI")
 		const url = blogLink;
-	  	if (e.ctrlKey){
-			window.open(url,'_blank')
-		} else {
-			document.location.href = url;
-		}
-	})
-	$('#button-project').on('click', function(e) {
-		const url = projectLink;
-	  	if (e.ctrlKey){
-			window.open(url,'_blank')
-		} else {
-			document.location.href = url;
-		}
-	})
-	$('#button-writeup').on('click', function(e) {
-		const url = writeupLink;
-	  	if (e.ctrlKey){
-			window.open(url,'_blank')
-		} else {
-			document.location.href = url;
-		}
-	})
-	$('#button-art').on('click', function(e) {
-		const url = artLink;
 	  	if (e.ctrlKey){
 			window.open(url,'_blank')
 		} else {

a95d706dbedce4b8a16ff118fa0999e14b27d487
Gomango999 <kv.zhu999@gmail.com>
2021-09-02 21:53:28 +1000

    Added basic symbol animation to the homepage

--- public/scripts/maid_dragon.js
+++ public/scripts/maid_dragon.js
@@ -1,6 +1,7 @@
 // https://shapecatcher.com/
 // https://qaz.wtf/u/convert.cgi?text=name.
 // https://gist.github.com/endolith/157796
+blank = "• • • • •"
 messages = [
   "♪ ⌒ ヮ ⌒ ♪",
   "# - _ - )",
@@ -22,18 +23,45 @@ function randomint(a, b) {
   return Math.random() * (b-a) + a;
 }
 
+function setSymbols(target, stage) {
+  target = target.split(" ");
+  for (let i = stage+2; i < target.length; i++) {
+    target[i] = "•";
+  }
+  newStr = target.join(" ")
+  $("#bottom-symbols").text(newStr);
+}
+
+let timeouts = []
+
 function rollSymbols(animation=true) {
-  reroll = true;
+  let reroll = true;
+  let str = "// ";
   while (reroll) {
-    let str = "// ";
+    str = "// ";
     let randIndex = Math.floor(randomint(0, messages.length));
     str += messages[randIndex];
 
+    reroll = true;
     if (str != $("#bottom-symbols").text()) {
-      // TODO: Add animations
-      $("#bottom-symbols").text(str);
       reroll = false;
     }
+  }
+
+  if (animation) {
+    // stop previous timeouts
+    while (timeouts.length != 0) {
+      clearTimeout(timeouts.pop());
+    }
+
+    // set new timeout
+    $("#bottom-symbols").text("// " + blank);
+    for (let i = 0; i < 5; i++) {
+      timeout = setTimeout(function(){setSymbols(str, i)}, (i+1)*1000);
+      timeouts.push(timeout);
+    }
+  } else {
+    $("#bottom-symbols").text(str);
   }
 }
 

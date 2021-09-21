3a4bad39d45740d81d5318677cae752dbe724299
Gomango999 <kv.zhu999@gmail.com>
2021-09-21 23:19:31 +1000

    Installed MongoDB

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,14 @@
+// const MongoClient = require('mongodb');
+//
+// const uri = "mongodb+srv://Kevin:sRuKWKefdvXm5xvc@cluster0.etgc3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 exports.index = function (req, res) {
     res.render('index.pug');
 }

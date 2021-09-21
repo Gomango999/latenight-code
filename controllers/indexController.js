88f45e96b65ddbbf1ce95c1d3678fb96ff3f002c
Gomango999 <kv.zhu999@gmail.com>
2021-09-22 00:03:07 +1000

    Added site counter to homepage

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,14 +1,35 @@
-// const MongoClient = require('mongodb');
-//
-// const uri = "mongodb+srv://Kevin:sRuKWKefdvXm5xvc@cluster0.etgc3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-//
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 exports.index = function (req, res) {
-    res.render('index.pug');
+  client.connect(err => {
+    if (err) throw err;
+
+    const collection = client.db("latenightcode").collection("main");
+
+    // find the main collection
+    collection.find({}).toArray(function(err, result) {
+      if (err) throw err;
+
+      // there should be only one document in the main collection, which stores
+      // all the information about the latenight code server.
+      result = result[0];
+      const visits = result.numSiteVisits;
+
+      // update the number of visits in the databse
+      collection.updateOne({}, {$set:{"numSiteVisits":visits+1}}).then(() => {
+        client.close();
+      });
+
+      // return the site for rendering
+      res.render('index.pug', {
+        visits: visits
+      });
+
+    });
+
+  });
+
 }

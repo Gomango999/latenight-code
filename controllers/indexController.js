43733181ccaf5cc1269fcc4d30b165832c2b637c
Gomango999 <kv.zhu999@gmail.com>
2021-09-23 00:12:54 +1000

    Made MongoDB database more reliable
    Replaced multiple connections with a single connection. There is already 
    a pool and timeout settings set up in a single connection, so I don't 
    have to manage connecting and unconnecting from the server.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,35 +1,36 @@
 const { MongoClient } = require('mongodb');
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoClient = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+var clientPromise = mongoClient.connect();
 
 exports.index = function (req, res) {
-  client.connect(err => {
-    if (err) throw err;
+  // get the document
+  var documentPromise = clientPromise
+  .then((client, err) => {
+    if (err) console.err(err);
 
+    // retrieve site statistics
     const collection = client.db("latenightcode").collection("main");
-
-    // find the main collection
-    collection.find({}).toArray(function(err, result) {
-      if (err) throw err;
-
-      // there should be only one document in the main collection, which stores
-      // all the information about the latenight code server.
-      result = result[0];
-      const visits = result.numSiteVisits;
-
-      // update the number of visits in the databse
-      collection.updateOne({}, {$set:{"numSiteVisits":visits+1}}).then(() => {
-        client.close();
-      });
-
-      // return the site for rendering
-      res.render('index.pug', {
-        visits: visits
-      });
-
-    });
-
+    return collection.find({}).toArray()
   });
 
+  // update the server and display the site
+  Promise.all([clientPromise, documentPromise])
+  .then(([client, documents], err) => {
+    if (err) console.err(err);
+    const document = documents[0];
+    const visits = document.numSiteVisits;
+
+    // render the site
+    res.render('index.pug', {
+      visits: visits+1
+    });
+
+    const collection = client.db("latenightcode").collection("main");
+    return collection.updateOne({}, {$set:{"numSiteVisits":visits+1}});
+  });
 }
